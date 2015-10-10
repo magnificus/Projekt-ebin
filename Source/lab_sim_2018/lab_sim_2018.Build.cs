b466100d7fd497fd3ab720bf8f648c61f790db53
unknown <tobiaselinder@gmail.com>
2015-10-10 19:41:36 +0200

    releaseversion

--- Source/lab_sim_2018/lab_sim_2018.Build.cs
+++ Source/lab_sim_2018/lab_sim_2018.Build.cs
@@ -6,21 +6,21 @@ public class lab_sim_2018 : ModuleRules
 {
 	public lab_sim_2018(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystem", "OnlineSubsystemNull", "OnlineSubsystemUtils" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        PrivateDependencyModuleNames.AddRange(new string[] { "OnlineSubsystem" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
 		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-		// if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
-		// {
-		//		if (UEBuildConfiguration.bCompileSteamOSS == true)
-		//		{
-		//			DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
-		//		}
-		// }
+		 PrivateDependencyModuleNames.Add("OnlineSubsystem");
+		 if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
+		 {
+				if (UEBuildConfiguration.bCompileSteamOSS == true)
+			{
+					DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
+				}
+		 }
 	}
 }
